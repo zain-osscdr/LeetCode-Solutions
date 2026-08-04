ccdb2297d3c1519e5f9c50c3152da22bf4b3a3c2
zain-osscdr <zainkhalidkhan2@gmail.com>
2026-08-04 20:05:19 +0500

    Time: 4 ms (88.18%), Space: 49.8 MB (42.1%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+                 if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+                map[nums[i]] = i;
+        }
+        return new int[] { };
+    }
+}
